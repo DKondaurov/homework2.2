7ed291241c6df26e06a233bd9fc00d526f9df0f4
homework <9874@bk.ru>
2021-10-28 10:25:42 +0300

    The original version

---
+++ src/pro/sky/java/course1/Bicycle.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course1;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
